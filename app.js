45fd968dc2dd2318365e17fc029e8c281c68a007
Trairat Punyawatthanunakool <samoooop@gmail.com>
2023-02-22 00:43:10 +0700

    Add redis session store

--- app.js
+++ app.js
@@ -5,60 +5,52 @@ const session = require("express-session");
 
 const port = 4000;
 
+let RedisStore = require("connect-redis")(session);
+
+// redis@v4
+const Redis = require("ioredis");
+
+let client = new Redis(process.env.REDIS_URL);
+
 app.use(express.static("public"));
 
 app.use(
-	session({
-		secret: "mySecret",
-		resave: false,
-		saveUninitialized: true,
-	})
+  session({
+    store: new RedisStore({ client }),
+    secret: process.env.FOO_COOKIE_SECRET ?? "test",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 app.use(function (req, res, next) {
-	if (!req.session.clickAmount) {
-		req.session.clickAmount = 0;
-	}
-	console.log("app's middleware invoked");
-	// get the url pathname
-	const pathname = parseurl(req).pathname;
-	console.log({ pathname });
-
-	if (pathname === "/get-click") {
-		req.session.clickAmount;
-	}
-	if (pathname === "/update-click") {
-		req.session.clickAmount += 1;
-	}
-	if (pathname === "/reset-click") {
-		req.session.clickAmount = 0;
-	}
-
-	next();
+  if (!req.session.clickAmount) {
+    req.session.clickAmount = 0;
+  }
+  next();
 });
 
 app.get("/", (req, res) => {
-	res.sendFile("/public/index.html");
+  res.sendFile("/public/index.html");
 });
 
 app.get("/get-click", function (req, res) {
-	const clickAmount = req.session.clickAmount;
-	console.log({ clickAmount });
-	res.send(JSON.stringify({ clickAmount: clickAmount }));
+  const clickAmount = req.session.clickAmount;
+  res.json({ clickAmount: clickAmount });
 });
 
 app.put("/update-click", function (req, res) {
-	const clickAmount = req.session.clickAmount;
-	console.log({ clickAmount });
-	res.send(JSON.stringify({ clickAmount: clickAmount }));
+  req.session.clickAmount += 1;
+  const clickAmount = req.session.clickAmount;
+  res.json({ clickAmount: clickAmount });
 });
 
 app.delete("/reset-click", function (req, res) {
-	const clickAmount = req.session.clickAmount;
-	console.log({ clickAmount });
-	res.send(JSON.stringify({ clickAmount: clickAmount }));
+  req.session.clickAmount = 0;
+  const clickAmount = req.session.clickAmount;
+  res.json({ clickAmount: clickAmount });
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
